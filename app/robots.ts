c120e765fca19029e3e7b0240385816d59144df2
EmailHoudini.Com <abede.lali.elrami@gmail.com>
2025-07-05 19:28:46 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/*', '/admin/*', '/_next/*'],
     },
-    sitemap: 'https://www.wipebg.ai/sitemap.xml',
+    sitemap: 'https://wipebg.ai/sitemap.xml',
   }
 } 
